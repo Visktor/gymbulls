b7d9150d986bb9e92e745b1cdd9cd6bc86247f3f
Vitor Barros <vitor-barros@DEV-RECIFE-0629.local>
2024-09-16 18:30:36 -0300

    feat: custom env

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -1,9 +1,10 @@
 import { z } from 'zod';
+import 'dotenv/config';
 
 const envSchema = z.object({
   PORT: z.coerce.number(),
   DB_HOST: z.string(),
-  DB_PORT: z.string(),
+  DB_PORT: z.coerce.number(),
   DB_NAME: z.string(),
   DB_USER: z.string(),
   DB_PASSWORD: z.string(),
@@ -14,3 +15,18 @@ export function validate(config: Record<string, unknown>) {
 }
 
 export type EnvConfig = z.infer<typeof envSchema>;
+
+const rawEnv = {
+  PORT: process.env.PORT,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+};
+
+export const appEnv = validate(rawEnv);
+
+// This is unfortunately necessary since there's no way to read NestJS's config
+// module when executing the typeorm migration scripts, so we create an env that
+// is globally available and can be imported from anywhere.
